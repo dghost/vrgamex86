bf1bd62543162cc18c71cf70c9eacff8e5ec60de
Luke Groeninger <luke@dghost.net>
2015-01-11 20:40:27 -0700

    Add cmake files

---
+++ CMakeFiles/vrgamex86.dir/DependInfo.cmake
@@ -0,0 +1,104 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/dghost/quake2vr/qcommon/shared/m_flash.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/__/qcommon/shared/m_flash.c.o"
+  "/Users/dghost/quake2vr/qcommon/shared/q_shared.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/__/qcommon/shared/q_shared.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_ai.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_ai.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_cmds.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_cmds.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_compress.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_compress.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_items.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_items.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_movement.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_movement.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_nodes.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_nodes.c.o"
+  "/Users/dghost/quake2vr/game/acesrc/acebot_spawn.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/acesrc/acebot_spawn.c.o"
+  "/Users/dghost/quake2vr/game/g_ai.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_ai.c.o"
+  "/Users/dghost/quake2vr/game/g_camera.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_camera.c.o"
+  "/Users/dghost/quake2vr/game/g_chase.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_chase.c.o"
+  "/Users/dghost/quake2vr/game/g_cmds.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_cmds.c.o"
+  "/Users/dghost/quake2vr/game/g_combat.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_combat.c.o"
+  "/Users/dghost/quake2vr/game/g_crane.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_crane.c.o"
+  "/Users/dghost/quake2vr/game/g_ctf.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_ctf.c.o"
+  "/Users/dghost/quake2vr/game/g_fog.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_fog.c.o"
+  "/Users/dghost/quake2vr/game/g_func.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_func.c.o"
+  "/Users/dghost/quake2vr/game/g_items.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_items.c.o"
+  "/Users/dghost/quake2vr/game/g_jetpack.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_jetpack.c.o"
+  "/Users/dghost/quake2vr/game/g_lights.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_lights.c.o"
+  "/Users/dghost/quake2vr/game/g_lock.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_lock.c.o"
+  "/Users/dghost/quake2vr/game/g_main.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_main.c.o"
+  "/Users/dghost/quake2vr/game/g_misc.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_misc.c.o"
+  "/Users/dghost/quake2vr/game/g_model.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_model.c.o"
+  "/Users/dghost/quake2vr/game/g_monster.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_monster.c.o"
+  "/Users/dghost/quake2vr/game/g_moreai.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_moreai.c.o"
+  "/Users/dghost/quake2vr/game/g_mtrain.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_mtrain.c.o"
+  "/Users/dghost/quake2vr/game/g_patchplayermodels.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_patchplayermodels.c.o"
+  "/Users/dghost/quake2vr/game/g_pendulum.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_pendulum.c.o"
+  "/Users/dghost/quake2vr/game/g_phys.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_phys.c.o"
+  "/Users/dghost/quake2vr/game/g_reflect.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_reflect.c.o"
+  "/Users/dghost/quake2vr/game/g_save.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_save.c.o"
+  "/Users/dghost/quake2vr/game/g_spawn.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_spawn.c.o"
+  "/Users/dghost/quake2vr/game/g_svcmds.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_svcmds.c.o"
+  "/Users/dghost/quake2vr/game/g_target.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_target.c.o"
+  "/Users/dghost/quake2vr/game/g_thing.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_thing.c.o"
+  "/Users/dghost/quake2vr/game/g_tracktrain.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_tracktrain.c.o"
+  "/Users/dghost/quake2vr/game/g_trigger.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_trigger.c.o"
+  "/Users/dghost/quake2vr/game/g_turret.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_turret.c.o"
+  "/Users/dghost/quake2vr/game/g_utils.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_utils.c.o"
+  "/Users/dghost/quake2vr/game/g_vehicle.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_vehicle.c.o"
+  "/Users/dghost/quake2vr/game/g_weapon.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/g_weapon.c.o"
+  "/Users/dghost/quake2vr/game/km_cvar.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/km_cvar.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_actor.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_actor.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_actor_weap.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_actor_weap.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_berserk.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_berserk.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_boss2.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_boss2.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_boss3.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_boss3.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_boss31.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_boss31.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_boss32.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_boss32.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_brain.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_brain.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_chick.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_chick.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_flipper.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_flipper.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_float.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_float.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_flyer.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_flyer.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_gladiator.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_gladiator.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_gunner.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_gunner.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_hover.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_hover.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_infantry.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_infantry.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_insane.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_insane.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_medic.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_medic.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_move.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_move.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_mutant.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_mutant.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_parasite.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_parasite.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_soldier.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_soldier.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_supertank.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_supertank.c.o"
+  "/Users/dghost/quake2vr/game/monster/m_tank.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/monster/m_tank.c.o"
+  "/Users/dghost/quake2vr/game/player/p_chase.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_chase.c.o"
+  "/Users/dghost/quake2vr/game/player/p_client.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_client.c.o"
+  "/Users/dghost/quake2vr/game/player/p_hud.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_hud.c.o"
+  "/Users/dghost/quake2vr/game/player/p_menu.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_menu.c.o"
+  "/Users/dghost/quake2vr/game/player/p_text.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_text.c.o"
+  "/Users/dghost/quake2vr/game/player/p_trail.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_trail.c.o"
+  "/Users/dghost/quake2vr/game/player/p_view.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_view.c.o"
+  "/Users/dghost/quake2vr/game/player/p_weapon.c" "/Users/dghost/quake2vr/game/CMakeFiles/vrgamex86.dir/player/p_weapon.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_FOG"
+  "NO_STEAM"
+  "OCULUS_DYNAMIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "external/SDL2/include"
+  "external/glew/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/vrgamex86.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vrgamex86.
+# This may be replaced when dependencies are built.
