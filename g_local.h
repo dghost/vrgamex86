bd90d6a109d637f5b3878d1be1c6599a215ed7e1
Luke Groeninger <luke@dghost.net>
2015-02-19 04:34:38 -0700

    Move game.h into q2vr main repo

--- g_local.h
+++ g_local.h
@@ -39,7 +39,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // short, server-visible gclient_t and edict_t structures,
 // because we define the full size ones in this file
 #define	GAME_INCLUDE
-#include "game.h"
+#include "../qcommon/shared/game.h"
 #include "player/p_menu.h"
 #include "player/p_text.h"
 #include "km_cvar.h"
